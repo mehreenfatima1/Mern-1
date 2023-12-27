97879dce82eece277d5d60df96f0b75c8b6e9def
Mehreen1 <mehreenyasir91@gmail.com>
2023-12-27 20:10:00 +0500

    added the third assignment

---
+++ import export ECMAscript/import.js
@@ -0,0 +1,1 @@
+import * as exp from "./index.js"
---
+++ import export ECMAscript/index.js
@@ -0,0 +1,9 @@
+export default function printing(){
+    for(var i=1;i<6;i++){
+        console.log("Count"+i)
+    }
+}
+printing()
+
+export var name="mehreen"
+console.log(name)
